ab6fbf3c1ddbc8ba987018e4b7bf378a4c064edb
melwin-bruun-nti-johanneberg <melwin.bruun@elev.ga.ntig.se>
2021-08-24 21:12:22 +0200

    stuff

--- lib/intro.ex
+++ lib/intro.ex
@@ -2,4 +2,42 @@ defmodule Intro do
   def hello do
     :world
   end
+
+  def smallest(n1,n2) when n1 < n2, do: n1
+  def smallest(_n1,n2), do: n2
+
+
+  def smallest(n1,n2,n3,n4), do: smallest(smallest(n1,n2),smallest(n3,n4))
+
+
+  def largest(n1,n2) when n1 > n2, do: n1
+  def largest(_n1,n2), do: n2
+  def largest(n1,n2,n3), do: largest(largest(n1,n2),n3)
+
+  def price(age) when age < 18, do: 10
+  def price(age) when age <= 64, do: 20
+  def price(_age), do: 15
+
+def next(input), do: input + 1
+
+def factorial(0), do: 1
+  def factorial(input) when input > 0, do: input * factorial(input - 1)
+
+
+def fibonacci(0), do: 0
+def fibonacci(1), do: 1
+def fibonacci(n), do: fibonacci(n-1)+fibonacci(n-2)
+
+def empty(list), do: length(list) == 0
+
+
+def first([head | _tail]), do: head
+
+
+
+
+
+
+
+
 end
--- test/intro_test.exs
+++ test/intro_test.exs
@@ -1,7 +1,79 @@
 defmodule IntroTest do
   use ExUnit.Case
 
-  test "greets the world" do
-    assert Intro.hello() == :world
-  end
+
+    test "smallest of two when smallest is first" do
+      assert Intro.smallest(1,2) == 1
+    end
+
+    test "smallest of two when smallest is last" do
+      assert Intro.smallest(3,2) == 2
+    end
+
+    test "smallest of two of equal size" do
+      assert Intro.smallest(1337,1337) == 1337
+    end
+
+    test "biggest of three when biggest is first" do
+      assert Intro.largest(3,2,1) == 3
+    end
+    test "biggest of three when biggest two are equal" do
+      assert Intro.largest(3,55,55) == 55
+    end
+
+    test "smallest of four when smallest is first" do
+      assert Intro.smallest(1,2,3,4) == 1
+    end
+
+    test "smallest of four when smallest is equal" do
+      assert Intro.smallest(1,2,1,45) == 1
+    end
+
+    test "Price for age 16" do
+      assert Intro.price(16) == 10
+    end
+
+    test "Price for age 30" do
+      assert Intro.price(30) == 20
+    end
+
+    test "Price for age 70" do
+      assert Intro.price(70) == 15
+    end
+
+    test "when inputs is 16 expect 17" do
+      assert Intro.next(16) == 17
+    end
+
+    test "when Fakultet is 3 expect 6" do
+      assert Intro.factorial(3) == 6
+    end
+
+    test "when Fakultet is 0 expect 1" do
+      assert Intro.factorial(0) == 1
+    end
+
+    test " when Fibonacci is 0 expect 0" do
+      assert Intro.fibonacci(0) == 0
+    end
+    test " when Fibonacci is 1 expect 1" do
+      assert Intro.fibonacci(1) == 1
+    end
+
+    #börjar på 0 räkningen
+    test " when Fibonacci is 5 expect 5" do
+      assert Intro.fibonacci(5) == 5
+    end
+
+      test "when list is empty exepct true" do
+        assert Intro.empty([]) == true
+      end
+
+      test "when list is not empty exepct false" do
+        assert Intro.empty([1]) == false
+      end
+
+      test "first element in list expect element" do
+        assert Intro.first([1,2,3]) == 1
+      end
 end
